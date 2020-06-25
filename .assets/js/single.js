293b3ebdcf8a6848401250e894f9c32154ac6332
Walker-Walker <awalker0@gmail.com>
2020-06-24 20:38:00 -0700

    added check for response header using .get(Link)

--- .assets/js/single.js
+++ .assets/js/single.js
@@ -1,4 +1,5 @@
 var issueContainerEl = document.querySelector("#issues-container");
+var limitWarningEl = document.querySelector("#limit-warning");
 
 var getRepoIssues = function(repo) {
    var apiUrl = "https://api.github.com/repos/"+repo+"/issues?direction=asc";
@@ -9,10 +10,14 @@ var getRepoIssues = function(repo) {
                displayIssues(data);
            });
        } 
-       else {
+      else {
            alert("There was a problem with your request!");
        }
-   });
+        // check if api has paginated issues
+        if (response.headers.get("Link")) {
+            displayWarning(repo);
+        }
+   });  
 };
 
 var displayIssues = function(issues) {
@@ -49,4 +54,16 @@ var displayIssues = function(issues) {
     }
 
 };
-getRepoIssues("facebook/react");
+
+var displayWarning = function(repo) {
+    //add text to warning container
+    limitWarningEl.textContent = "To see more than 30 issues, visit";
+    var linkEl = document.createElement("a");
+    linkEl.textContent = "See More Issues on Github.com";
+    linkEl.setAttribute("href", "https://github.com/" + repo + "/issues");
+    linkEl.setAttribute("target", "_blank");
+
+    // append to warning container
+    limitWarningEl.appendChild(linkEl);
+}
+getRepoIssues("angular/angular");
